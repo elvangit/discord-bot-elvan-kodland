02a51c73647141744a5e18b450e06a49500dc883
elvangit <146006949+elvangit@users.noreply.github.com>
2023-11-21 20:16:14 +0700

    Add files via upload

--- pass_generator.py
+++ pass_generator.py
@@ -1,8 +1,10 @@
 import random
 
-def gen_pass(pass_length):
-    elements = "=-`[]\;',./abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-    password = ""
-    for i in range(pass_length):
-        password += random.choice(elements)
-    return password
+sandi =  "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+panjang = int(input("Berapa panjang karakter untuk kata sandi   "))
+hasil = ""
+
+for i in range(panjang):
+    hasil += random.choice(sandi)
+print("Kata sandi yang dihasilkan adalah",hasil)    
+
